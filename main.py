351881bfcc4855dfcc34043b231dd9ee6d40d7e9
euiyounghwang <marieuig@gmail.com>
2023-11-06 12:27:54 -0600

    Add Swagger API

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from config.log_config import create_log
 
+logger = create_log()
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 ''' http://localhost:7777/docs '''
 
@@ -9,4 +22,27 @@ app = FastAPI()
          description="Default GET API", 
          summary="Return Json")
 async def root():
+    logger.info('root')
     return {"message": "Hello World"}
+
+
+@app.get("/test", tags=['API'],  
+         status_code=200,
+         description="Default GET Param API", 
+         summary="Return GET Param Json")
+async def root_with_arg(id):
+    logger.info('root_with_arg - {}'.format(id))
+    return {"message": "Hello World [{}]".format(id)}
+
+
+@app.get("/test/{id}", tags=['API'],  
+         status_code=200,
+         description="Default GET with Body API", 
+         summary="Return GET with Body Json")
+async def root_with_param(id):
+    logger.info('root_with_arg - {}'.format(id))
+    return {"message": "Hello World [{}]".format(id)}
+
+
+# router
+# app.include_router(es_search_controller.app, tags=["Search"], )
