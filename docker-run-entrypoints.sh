2a504fa1918bc961bed0c4330249588cf1604ebf
euiyounghwang <marieuig@gmail.com>
2024-01-04 19:29:48 -0600

    Update

--- docker-run-entrypoints.sh
+++ docker-run-entrypoints.sh
@@ -8,5 +8,10 @@ set -ex
 # --
 source /app/poetry-venv/bin/activate
 cd /app/FN-Basic-Services
+
+# --
+# waiting for ES
+./wait_for_es.sh $ES_HOST
+
 # uvicorn main:app --reload --host=0.0.0.0 --port=7777 --workers 4
 poetry run uvicorn main:app --host=0.0.0.0 --port=7777 --workers 4
