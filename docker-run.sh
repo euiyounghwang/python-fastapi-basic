7fd116b8432f2be01b240cbeda09cd6fd5c4b8ea
euiyounghwang <marieuig@gmail.com>
2024-02-26 21:05:59 -0600

    Update elasticsearch url

--- docker-run.sh
+++ docker-run.sh
@@ -7,7 +7,7 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 docker run --rm --platform linux/amd64 -it -d \
   --name fn-basic-api --publish 17777:7777 --expose 7777 \
   --network bridge \
-  -e ES_HOST=http://host.docker.internal:9209 \
+  -e ES_HOST=http://host.docker.internal:9203 \
   -v "$SCRIPTDIR:/app/FN-Basic-Services/" \
   fn-django-search-api:es
 
